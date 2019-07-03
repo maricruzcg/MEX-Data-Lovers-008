b86a7ff0ecfb750f7bb7bef14ebd0195a352aaad
TeresaFlores <teresaflores1730@gmail.com>
2019-07-03 18:28:33 -0500

    Formula para mostrar los pokemon tipo tarjeta en main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 let mostarPokemon= POKEMON.pokemon;
-let informacion= document.getElementById("root");
-
+let information= document.getElementById("contenido");
+//Mostrando tarjetas de pokemon
 mostarPokemon.forEach(function(element)  {
-    console.log('lol');
-    
-  //document.write(element.type);
-  //document.write(element.num)
-  informacion.insertAdjacentHTML("beforeEnd", <p>${element.num}</p>)
+  //Diseño de tarjetas y contenido
+  let cards = `<div>
+  <img src="${element.img}">
+  <p>No.${element.num}</p>
+  <p>${element.name}</p>
+  <p>${element.type}</p>
+  <p>Debilidad: ${element.weaknesses}</p>
+</div>`
+//Llamando variable para mostrar
+  information.insertAdjacentHTML("beforeEnd", cards);
+});
 
-})
+//Funcion para filtrar por tipo
+const pasto= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Grass"));
+console.log(pasto)
+const 
 
-console.log(informacion)
+//let filterType= mostarPokemon.filter(pokemon => (pokemon.type(Grass)));
+//console.log(filterType);
 
