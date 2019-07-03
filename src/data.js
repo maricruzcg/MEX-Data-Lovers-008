18030e3cb79f28766714dc67b828a53b299eba11
maricruzcg <maricruz7@live.com.mx>
2019-07-03 18:34:01 -0500

    Subiendo html responsive

--- src/data.js
+++ src/data.js
@@ -1,8 +1,20 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+const mostrar = () => {
+  let pokemon = POKEMON.pokemon;
+  for(i = 0; i <= pokemon.length; i ++){
+    let number = pokemon[i].num;
+    let namePokemon = pokemon[i].name;
+    let typePokemon = pokemon[i].type;
 
-window.example = example;
+    console.log(number);
+    console.log(namePokemon);
+    document.write(typePokemon);
+
+  }
+  
+};
+mostrar();
+
+window.mostrar = mostrar;
